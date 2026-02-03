f51280e4f45edb3409003ed51e71076a54b845ad
Issa Khafaga <ikhafaga@live.com>
2026-02-03 14:44:56 +0000

    feat: add update methods for project images repo

--- lib/project_image_repository.py
+++ lib/project_image_repository.py
@@ -1,7 +1,8 @@
 from lib.project_image import ProjectImage;
+from lib.database_connection import DatabaseConnection
 
 class ProjectImageRepository:
-    def __init__(self, db_connection):
+    def __init__(self, db_connection: DatabaseConnection):
         self._connection = db_connection
 
     def post_image(self, project_image: ProjectImage) -> ProjectImage:
@@ -24,7 +25,7 @@ class ProjectImageRepository:
                 id,
             ],
         )
-        row: list = rows[0]
+        row: dict = rows[0]
         return ProjectImage(
             caption=row["caption"],
             image_url=row["image_url"],
@@ -49,9 +50,18 @@ class ProjectImageRepository:
         return project_images_list
 
     def delete_project_image(self, id: int) -> None:
-        rows = self._connection.execute(
+        self._connection.execute(
             "DELETE FROM project_images WHERE id = %s",
             [
                 id,
             ],
         )
+    def update_image_url(self, id: int, new_image_url: str) -> ProjectImage:
+        rows = self._connection.execute("UPDATE project_images SET image_url = %s WHERE id = %s RETURNING *", [new_image_url,id,])
+        row: dict = rows[0]
+        return ProjectImage(caption=row['caption'], image_url=row['image_url'], id=row["id"], project_id=row["project_id"])
+    
+    def update_caption(self, id: int, new_caption: str) -> ProjectImage:
+        rows: list[dict] = self._connection.execute("UPDATE project_images SET image_url = %s WHERE id = %s RETURNING *", [new_caption,id,])
+        row: dict = rows[0]
+        return ProjectImage(caption=row['caption'], image_url=row['image_url'], id=row["id"], project_id=row["project_id"])
