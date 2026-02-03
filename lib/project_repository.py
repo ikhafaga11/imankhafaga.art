582e4a3b670d7d0c07f44e5cb1fe746b9f6b3256
Issa Khafaga <ikhafaga@live.com>
2026-02-03 14:21:56 +0000

    feat: add get project image method

--- lib/project_repository.py
+++ lib/project_repository.py
@@ -22,7 +22,9 @@ class ProjectRepository:
         return project
 
     def get_project(self, id: int) -> Project:
-        rows = self._connection.execute("SELECT * FROM projects WHERE id = %s", [id])
+        rows = self._connection.execute(
+            "SELECT * FROM projects WHERE id = %s RETURNING *;", [id]
+        )
         row = rows[0]
         return Project(row["title"], row["cover_image_url"], row["id"])
 
@@ -71,13 +73,28 @@ class ProjectRepository:
 
     def post_image(self, project_image: ProjectImage) -> ProjectImage:
         rows = self._connection.execute(
-            "INSERT INTO project_image (caption, image_url, project_id) VALUES( %s %s %s) RETURNING id;"
+            "INSERT INTO project_images (caption, image_url, project_id) VALUES( %s %s %s) RETURNING id;",
             [
                 project_image.caption,
                 project_image.image_url,
                 project_image.project_id,
-            ]
+            ],
         )
         project_image.id = rows[0]["id"]
 
         return project_image
+
+    def get_poject_image(self, id: int) -> ProjectImage:
+        rows = self._connection.execute(
+            "SELECT * FROM project_images WHERE id = %s RETURNING *; ",
+            [
+                id,
+            ],
+        )
+        row = rows[0]
+        return ProjectImage(
+            caption=row["caption"],
+            image_url=row["image_url"],
+            id=row["id"],
+            project_id=row["project_id"],
+        )
