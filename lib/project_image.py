72b1420a62bc9b37ee6dbbc2a9f10296a5e934f7
Issa Khafaga <ikhafaga@live.com>
2026-02-03 01:19:00 +0000

    chore: update model to include id and project id

--- lib/project_image.py
+++ lib/project_image.py
@@ -1,10 +1,12 @@
 class ProjectImage:
-    def __init__(self, caption, image_url):
+    def __init__(self, caption, image_url, id=None, project_id=None):
+        self.id = id
         self.caption = caption
         self.image_url = image_url
+        self.project_id = project_id
 
     def __repr__(self):
-        return f"ProjectImage({self.caption}, {self.image_url})"
+        return f"ProjectImage({self.id}, {self.caption!r}, {self.image_url!r}, {self.project_id})"
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
