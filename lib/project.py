81dfa93f6a5c711c1fba61f54a4579c38a2709ea
Issa Khafaga <ikhafaga@live.com>
2026-02-03 01:11:23 +0000

    feat: update project model to include id

--- lib/project.py
+++ lib/project.py
@@ -1,11 +1,12 @@
 class Project:
-    def __init__(self, title, cover_image_url):
+    def __init__(self, title, cover_image_url, id=None):
+        self.id = id
         self.title = title
         self.cover_image_url = cover_image_url
         self.images = []
 
     def __repr__(self):
-        return f"Project({self.title}, {self.cover_image_url}, {self.images})"
+        return f"Project({self.id}, {self.title}, {self.cover_image_url}, {self.images})"
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
