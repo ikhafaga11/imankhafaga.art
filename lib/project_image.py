a9b76b230d58e0e249904a8149301937c5016582
Issa Khafaga <ikhafaga@live.com>
2026-02-03 00:49:20 +0000

    feat: add project_image repo

---
+++ lib/project_image.py
@@ -0,0 +1,10 @@
+class ProjectImage:
+    def __init__(self, caption, image_url):
+        self.caption = caption
+        self.image_url = image_url
+    
+    def __repr__(self):
+        return f"ProjectImage({self.caption}, {self.image_url})"
+    
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
