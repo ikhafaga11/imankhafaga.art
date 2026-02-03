a5928f309e6b092a80e3400fa19692f59f87cf97
Issa Khafaga <ikhafaga@live.com>
2026-02-03 16:25:31 +0000

    feat: add sketchbook model and repo with add and delete methods

--- lib/sketchbook.py
+++ lib/sketchbook.py
@@ -1,10 +1,10 @@
 class Sketchbook:
-    def __init__(self, image_url:str, id=None) -> None:
+    def __init__(self, image_url: str, id=None) -> None:
         self.id = id
         self.image_url = image_url
 
     def __repr__(self):
         return f"Sketchbook({self.id}, {self.image_url!r})"
-    
+
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
---
+++ lib/sketchbook_repository.py
@@ -0,0 +1,25 @@
+from lib.sketchbook import Sketchbook
+from lib.database_connection import DatabaseConnection
+
+
+class SketchbookRepository:
+    def __init__(self, db_connection: DatabaseConnection):
+        self._connection = db_connection
+
+    def add_sketch(self, image_url: str) -> Sketchbook:
+        rows: list[dict] = self._connection.execute(
+            "INSERT INTO sketchs (image_url) VALUES ( %s ) RETURNING *",
+            [
+                image_url,
+            ],
+        )
+        row: dict = rows[0]
+        return Sketchbook(image_url=row["image_url"], id=row["id"])
+
+    def delete_sketch(self, id: int) -> None:
+        self._connection.execute(
+            "DELETE FROM sketchs WHERE id = %s",
+            [
+                id,
+            ],
+        )
