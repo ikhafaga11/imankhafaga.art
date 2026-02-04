ab63bd2e79c67bedeb54740c97dda054ee7b7fce
Issa Khafaga <ikhafaga@live.com>
2026-02-04 01:50:05 +0000

    chore: updated to test updated sql table names

--- test/test_database_connection.py
+++ test/test_database_connection.py
@@ -3,13 +3,14 @@ def test_database_connection_projects_table(db_connection):
     result = db_connection.execute("SELECT * FROM projects")
     assert len(result) == 2
 
-    
+
 def test_database_connection_project_images_table(db_connection):
     db_connection.seed("seeds/database_connection.sql")
-    result = db_connection.execute("SELECT * FROM project_images WHERE id = 1")
+    result = db_connection.execute("SELECT * FROM project_contents WHERE id = 1")
     assert len(result) == 1
+
 
 def test_database_connection_sketchs_table(db_connection):
     db_connection.seed("seeds/database_connection.sql")
-    result = db_connection.execute("SELECT * FROM sketchs")
+    result = db_connection.execute("SELECT * FROM sketches")
     assert len(result) == 1
