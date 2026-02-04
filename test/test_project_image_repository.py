52c49402c94daa81bc87c3a8170d3bdb5d385302
Issa Khafaga <ikhafaga@live.com>
2026-02-04 01:51:05 +0000

    chore: updated to use new repo naming convention

--- test/test_project_image_repository.py
+++ test/test_project_image_repository.py
@@ -1,8 +1,9 @@
-from lib.project_image_repository import ProjectImageRepository
+from lib.project_content_repository import ProjectContentRepository
+
 
 def test_get_all_project_images(db_connection):
-    db_connection.seed('seeds/database_connection.sql')
-    repo = ProjectImageRepository(db_connection)
-    results = repo.get_all_project_images(1)
-    
+    db_connection.seed("seeds/database_connection.sql")
+    repo = ProjectContentRepository(db_connection)
+    results = repo.get_project_contents(1)
+
     assert len(results) == 1
