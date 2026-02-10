4dda284fcfcfb5f6c7ceac046d61b87f702eef0f
Issa Khafaga <ikhafaga@live.com>
2026-02-10 22:41:01 +0000

    chore: load dot env and remove duplicate mail sender app config

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from flask_mail import Mail, Message
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 app.config["MAIL_SERVER"] = "sandbox.smtp.mailtrap.io"
 app.config["MAIL_PORT"] = 2525
@@ -18,7 +19,6 @@ app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USE_SSL"] = False
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
-app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_DEFAULT_SENDER"] = "no-reply@iman-portfolio.dev"
 
 mail = Mail(app)
