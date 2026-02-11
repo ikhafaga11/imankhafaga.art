2c8ea7115fb769eecaf826ea7dfab63c227b55e7
Issa Khafaga <ikhafaga@live.com>
2026-02-11 18:59:41 +0000

    chore: update dunmmy email to gmail for contact form

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
-app.config["MAIL_SERVER"] = "sandbox.smtp.mailtrap.io"
-app.config["MAIL_PORT"] = 2525
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USE_SSL"] = False
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"]=True
+app.config["MAIL_USE_SSL"]=False
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
-app.config["MAIL_DEFAULT_SENDER"] = "no-reply@iman-portfolio.dev"
+app.config["MAIL_DEFAULT_SENDER"] = ("Iman Artist Website", os.getenv("MAIL_DEFAULT_SENDER"))
 
 mail = Mail(app)
 
