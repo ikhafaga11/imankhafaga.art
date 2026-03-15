6208f97689508d730424725dd92cbc0e40b55bf7
Issa Khafaga <ikhafaga@live.com>
2026-03-15 00:49:12 +0000

    feat: add backToTop btn

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+const backToTop = document.getElementById("backToTop")
+
+window.addEventListener('scroll',() => {
+    scrollY > 300 ? backToTop.style.display = "block" : backToTop.style.display = "none"
+})
+
+backToTop.addEventListener('click', () => {
+window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+})
+})
